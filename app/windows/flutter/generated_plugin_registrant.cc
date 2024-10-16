d0226b981582b08e12fb3644277d014b229b4150
ahsanbulbul <ahsanbulbul>
2024-10-16 16:13:33 +0600

    Turn flash on/off using vol high-low

--- app/windows/flutter/generated_plugin_registrant.cc
+++ app/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <geolocator_windows/geolocator_windows.h>
+#include <torch_flashlight/torch_flashlight_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
+  TorchFlashlightPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("TorchFlashlightPluginCApi"));
 }
