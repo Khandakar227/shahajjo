63ec5efbdc16f5f2b87d6d5c211585aea1942ce1
Tahsinul Islam Rupom <tahsinulislam21@iut-dhaka.edu>
2024-11-15 04:28:52 +0600

    incident_monitor.dart view update

--- app/lib/views/incident_monitor.dart
+++ app/lib/views/incident_monitor.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:shahajjo/components/app_bar.dart';
+import 'package:shahajjo/components/incident_map.dart';
 
 class IncidentMonitorPage extends StatefulWidget {
   const IncidentMonitorPage({super.key, required this.title});
   final String title;
 
   @override
-  _IncidentMonitorState createState() => _IncidentMonitorState();
+  State<IncidentMonitorPage> createState() => _IncidentMonitorPageState();
 }
 
-class _IncidentMonitorState extends State<IncidentMonitorPage> {
+class _IncidentMonitorPageState extends State<IncidentMonitorPage> {
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
     const double appBarHeight = kToolbarHeight;
-
     return Scaffold(
         appBar: MyAppbar(title: widget.title),
         body: SafeArea(
             child: SizedBox(
-          height: screenHeight -
-              appBarHeight, // Adjust the height to exclude the app bar
-          child: const SizedBox(),
+          height: screenHeight - appBarHeight,
+          child: const IncidentMonitorMap(),
         )));
   }
 }
